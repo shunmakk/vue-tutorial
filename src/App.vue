0cab37afc039183c4d22a2128b3bfe27ffbf5e2b
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-18 20:49:14 +0900

    とりあえず超基礎

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+console.log("Hello World!");
+const message = "Hello World";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Vue3 入門</h1>
+  <p>{{ message }}</p>
+  <div>{{ message.length * 100 }}</div>
+  <div>{{ message.length > 10 ? "long" : "short" }}</div>
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -26,5 +22,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
